7c69f1d88869a29c59f240525bdff1fa78d96cb9
LeventJ <Levent_J@163.com>
2015-05-01 18:39:43 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Employee.h"
+#include "Sales.h"
+using namespace std;
+
+int main()
+{
+    Employee emp("Tom");
+    Sales sal("Jerry");
+
+    emp.setlevel(2);
+    sal.setlevel(3);
+
+    emp.setcount(200);
+    sal.setcount(200);
+
+/**
+ * Employer 的 Salary 计算方式：count*500+level*1000
+ * Employer 的 Salary 计算方式：count*800+level*800;
+ */
+
+    cout<<"Employer:"<<emp.getname()<<"  level:"<<emp.getlevel()<< "  salary:"<<emp.getsalary()<<endl;
+    cout<<"Sales:"<<sal.getname()<<"  level:"<<sal.getlevel()<<"  salary:"<<sal.getsalary()<<endl;
+    return 0;
+}
